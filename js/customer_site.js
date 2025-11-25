e6e719fb2ba39d953ccb9aae52bf237932aefe77
xxch9527 <116860389+xxch9527@users.noreply.github.com>
2025-11-25 16:56:53 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,27 +2,27 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-    },
-    ikun: {
-        api: 'https://ikunzyapi.com/api.php/provide/vod',
-        name: 'ikun',
-    },
-    liangzi: {
-        api: 'https://cj.lziapi.com/api.php/provide/vod',
-        name: '量子',
-    },
-    baofeng: {
-        api: 'https://bfzyapi.com/api.php/provide/vod',
-        name: '暴风',
-    },
-    taohua: {
-        api: 'https://thzy1.me/api.php/provide/vod',
-        name: '桃花',
-    },
-    senlin: {
-        api: 'https://slapibf.com/api.php/provide/vod',
-        name: '森林',
     }
+    // ikun: {
+    //     api: 'https://ikunzyapi.com/api.php/provide/vod',
+    //     name: 'ikun',
+    // },
+    // liangzi: {
+    //     api: 'https://cj.lziapi.com/api.php/provide/vod',
+    //     name: '量子',
+    // },
+    // baofeng: {
+    //     api: 'https://bfzyapi.com/api.php/provide/vod',
+    //     name: '暴风',
+    // },
+    // taohua: {
+    //     api: 'https://thzy1.me/api.php/provide/vod',
+    //     name: '桃花',
+    // },
+    // senlin: {
+    //     api: 'https://slapibf.com/api.php/provide/vod',
+    //     name: '森林',
+    // }
 };
 
 // 调用全局方法合并
